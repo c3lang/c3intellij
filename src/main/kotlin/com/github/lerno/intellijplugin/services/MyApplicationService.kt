86feb916354f9786d47b9d0ab6195140db8ecd7e
GitHub Action <action@github.com>
2020-08-27 07:54:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/lerno/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lerno/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lerno.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lerno.intellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/lerno/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lerno/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lerno.intellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lerno.intellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
