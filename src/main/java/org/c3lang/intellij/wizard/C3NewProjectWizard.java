333f459fd2225c72c98adcc0d8026ce2d3ec409d
= <chaosbray@gmail.com>
2025-04-03 01:42:07 +0200

    Fixed Project Wizards
    - removed usages of UIWizardUtil.setupProjectFromBuilder and StartupManager.runAfterOpened

--- src/main/java/org/c3lang/intellij/wizard/C3NewProjectWizard.java
+++ src/main/java/org/c3lang/intellij/wizard/C3NewProjectWizard.java
@@ -3,15 +3,10 @@ package org.c3lang.intellij.wizard;
 import com.intellij.ide.wizard.AbstractNewProjectWizardStep;
 import com.intellij.ide.wizard.NewProjectWizardStep;
 import com.intellij.ide.wizard.language.LanguageGeneratorNewProjectWizard;
-import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.module.WebModuleBuilder;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiManager;
+import org.apache.commons.lang3.Validate;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.c3lang.intellij.C3Icons;
@@ -19,11 +14,9 @@ import org.c3lang.intellij.C3Util;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Objects;
-
-import static com.intellij.ide.wizard.UIWizardUtil.setupProjectFromBuilder;
 
 public class C3NewProjectWizard implements LanguageGeneratorNewProjectWizard
 {
@@ -68,7 +61,9 @@ public class C3NewProjectWizard implements LanguageGeneratorNewProjectWizard
         @Override
         public void setupProject(@NotNull Project project)
         {
-            String[] dirs = new String[] {
+            Validate.notNull(project.getBasePath(), "Project base path cannot be null");
+
+            String[] dirs = new String[]{
                     "build",
                     "docs",
                     "lib",
@@ -78,45 +73,31 @@ public class C3NewProjectWizard implements LanguageGeneratorNewProjectWizard
                     "test"
             };
 
-            File licenseFilePath = Path.of(Objects.requireNonNull(project.getBasePath()), "LICENSE").toFile();
-            File readmeFilePath = Path.of(Objects.requireNonNull(project.getBasePath()), "README.md").toFile();
-            File projectJsonFilePath = Path.of(Objects.requireNonNull(project.getBasePath()), "project.json").toFile();
-            File mainFilePath = Path.of(Objects.requireNonNull(project.getBasePath()), "src/main.c3").toFile();
+            File licenseFilePath = Path.of(project.getBasePath(), "LICENSE").toFile();
+            File readmeFilePath = Path.of(project.getBasePath(), "README.md").toFile();
+            File projectJsonFilePath = Path.of(project.getBasePath(), "project.json").toFile();
+            File mainFilePath = Path.of(project.getBasePath(), "src/main.c3").toFile();
 
             WebModuleBuilder<?> builder = new WebModuleBuilder<>();
             builder.setName(parent.getContext().getProjectName());
             builder.setContentEntryPath(parent.getContext().getProjectFileDirectory());
-            setupProjectFromBuilder(this, project, builder);
+            builder.commit(project);
 
-            StartupManager startupManager = StartupManager.getInstance(project);
-            startupManager.runAfterOpened(() -> {
-                String contentEntryPath = builder.getContentEntryPath();
-                if (contentEntryPath == null) {
-                    return;
-                }
-                VirtualFile root = LocalFileSystem.getInstance().findFileByPath(contentEntryPath);
-                if (root == null) {
-                    return;
-                }
+            for (String dir : dirs)
+            {
+                FileUtil.createDirectory(Path.of(project.getBasePath(), dir).toFile());
+            }
 
-                WriteCommandAction.runWriteCommandAction(project, null, null, () -> {
-                    PsiDirectory directory = PsiManager.getInstance(project).findDirectory(root);
-                    if (directory != null) {
-                        for (String dir : dirs) {
-                            FileUtil.createDirectory(Path.of(Objects.requireNonNull(project.getBasePath()), dir).toFile());
-                        }
-
-                        try {
-                            FileUtil.writeToFile(licenseFilePath, "");
-                            FileUtil.writeToFile(readmeFilePath, "");
-                            C3Util.writeToFile(project.getName(), "templates/project.json", projectJsonFilePath);
-                            C3Util.writeToFile(project.getName(), "templates/main", mainFilePath);
-                        } catch (IOException e) {
-                            log.error(e.getMessage(), e);
-                        }
-                    }
-                });
-            });
+            try
+            {
+                FileUtil.writeToFile(licenseFilePath, "");
+                FileUtil.writeToFile(readmeFilePath, "");
+                C3Util.writeToFile(project.getName(), "templates/project.json", projectJsonFilePath);
+                C3Util.writeToFile(project.getName(), "templates/main", mainFilePath);
+            } catch (IOException e)
+            {
+                log.error(e.getMessage(), e);
+            }
         }
     }
 }
