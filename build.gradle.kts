856f15cee83ef39730975e28867be6825f4e5409
Thomas <thomas.blakely@gmx.com>
2026-01-28 19:52:50 +1300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -175,8 +175,11 @@ tasks {
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
-    }
+        channels.set(properties("pluginVersion").map { version ->
+            val channel = version.split('-').getOrNull(1)?.split('.')?.firstOrNull() ?: "default"
+            listOf(channel)
+        })
+    }	
 
     compileJava {
         dependsOn(generateC3Lexer)
